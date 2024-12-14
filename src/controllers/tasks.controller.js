8f078f9473f7b1b46b74ef8f64d9a466476f74c3
juanchocusi <juanchocusi@gmail.com>
2024-12-14 07:27:52 -0500

    controller bueno

---
+++ src/controllers/tasks.controller.js
@@ -0,0 +1,71 @@
+import { pool } from "../db.js";
+
+export const getAllTask = async (req, res) => {
+  try {
+    const [result] = await pool.query(
+      "SELECT * FROM tasks order by createAt ASC"
+    );
+    res.json(result);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export const getTask = async (req, res) => {
+  try {
+    const [result] = await pool.query("select * from tasks where id = ?", [
+      req.params.id,
+    ]);
+
+    if (result.length === 0) {
+      return res.status(400).json({ message: "Taks not foud..." });
+    }
+    res.json(result[0]);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export const createTask = async (req, res) => {
+  try {
+    const { title, description } = req.body;
+    const [result] = await pool.query(
+      "INSERT INTO tasks (title, description) VALUES (?, ?)",
+      [title, description]
+    );
+    res.json({
+      id: result.insertId,
+      title,
+      description,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export const updateTask = async (req, res) => {
+  try {
+    const result = await pool.query("update tasks set ? where id = ?", [
+      req.body,
+      req.params.id,
+    ]);
+    res.json(result);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export const deleteTask = async (req, res) => {
+  try {
+    const [result] = await pool.query("delete from tasks where id = ?", [
+      req.params.id,
+    ]);
+
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ message: "Eiminado" });
+    }
+    return res.sendStatus(204);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log("hello my world.. ")
+import express from "express";
+import { PORT } from "./config.js";
+/* import indexRoutes from "./routes/index.routes.js"; */
+import taskRoutes from "./routes/task.routes.js";
+import cors from 'cors'
+import morgan from "morgan";
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
+
+/* app.use(indexRoutes); */
+app.use(taskRoutes);
+
+app.listen(PORT);
+
+console.log(`Server corriendo en el puerto: ${PORT}`);
